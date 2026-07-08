f2d975dc087ef18667fb9c91350ccc403ad5f678
manrenas <renatopontoni.rocha@gmail.com>
2026-07-08 02:35:19 +0100

    Delete js directory

--- js/modules/blog.js
+++
@@ -1,7 +0,0 @@
-export function iniciarBlog() {
-    const cards = document.querySelectorAll('.blog-card');
-    cards.forEach((card, index) => {
-        card.style.animationDelay = `${index * 80}ms`;
-        card.classList.add('fade-in');
-    });
-}
